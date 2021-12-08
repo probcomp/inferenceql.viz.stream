8de3c32bec4eaaa7958b47762cc48d5b69672609
Harish Tella <htella@mit.edu>
2021-12-08 17:34:00 -0600

    use numerical ranges from config

--- src/inferenceql/viz/stream/panels/viz/dashboard.cljs
+++ src/inferenceql/viz/stream/panels/viz/dashboard.cljs
@@ -4,17 +4,14 @@
             [goog.string :refer [format]]
             [vega-embed$vega :as vega]
             [goog.object]
+            [inferenceql.viz.config :refer [config]]
             [inferenceql.viz.stream.panels.viz.util :refer [filtering-summary
                                                             obs-data-color virtual-data-color
                                                             unselected-color vega-type-fn
                                                             vl5-schema]]))
 
-(def ranges {:BMI [-15 65]
-             :labtest_1 [0 15.514786623448469]
-             :labtest_2 [0 11390.127776197322]
-             :labtest_3 [0 16.747923871194022]
-             :labtest_4 [0 11056.229850297572]
-             :heart_rate [0 295.7882627553621]})
+(def ranges
+  (get-in config [:settings :numerical_ranges]))
 
 (defn bin-counts
   "Takes a seq of numerical `data` and `binning`. Returns the number of data points in each bin.

9ac2bfee0b6438b289b47c435a013515ad1b1fe6
Harish Tella <htella@mit.edu>
2021-12-16 12:10:49 -0600

    fix: column selection for x and y axis on inference plots
    * mimics selection in other plots like strip plots and table bubble plots.

--- src/inferenceql/viz/stream/panels/viz/inferences.cljs
+++ src/inferenceql/viz/stream/panels/viz/inferences.cljs
@@ -5,16 +5,18 @@
             [vega-embed$vega :as vega]
             [clojure.math.combinatorics :refer [combinations]]
             [inferenceql.viz.stream.store :refer [schema]]
+            [medley.core :as medley]
             [goog.object]
             [inferenceql.viz.config :refer [config]]
-            [inferenceql.viz.stream.panels.viz.util :refer [filtering-summary
-                                                            obs-data-color virtual-data-color
-                                                            unselected-color vega-type-fn
-                                                            vl5-schema]]))
-
+            [inferenceql.viz.stream.panels.viz.util :refer [vl5-schema]]))
 
 (def ranges
   (get-in config [:settings :numerical_ranges]))
+
+(def options-count
+  "Map of nominal column name to number of options"
+  (->> (get-in config [:transitions :options])
+    (medley/map-vals count)))
 
 (defn vega-type [schema col]
   (let [iql-type (get schema col)
@@ -24,30 +26,50 @@
     (get vega-type-map iql-type)))
 
 (defn inference-plot [[c1 c2]]
-  (let [c1-type (vega-type schema c1)
-        c2-type (vega-type schema c2)]
-    {:mark {:type "rect" :tooltip true}
-     :width (case c1-type
-              "nominal" {:step 20},
-              "quantitative" 240)
-     :height (case c2-type
-              "nominal" {:step 20},
-              "quantitative" 240),
-     :encoding {:x {:bin (case c1-type
-                           "nominal" false
-                           "quantitative" {:maxbins 50
-                                           :extent (get ranges c1)})
-                    :field (name c1),
-                    :type c1-type}
-                :y {:bin (case c2-type
-                           "nominal" false
-                           "quantitative" {:maxbins 50}),
-                                           :extent (get ranges c2)
-                    :field (name c2),
-                    :type c2-type}
-                :color {:aggregate "count",
-                        :type "quantitative"
-                        :legend nil}}}))
+  ;; First we determine if we need to swap the x and y columns.
+  (let [col-types (map #(vega-type schema %) [c1 c2])
+        [c1 c2] (case col-types
+                  ["quantitative" "quantitative"]
+                  [c1 c2]
+
+                  ["nominal" "quantitative"]
+                  ;; Quantitative should be on the x-axis.
+                  [c2 c1]
+
+                  ["quantitative" "nominal"]
+                  [c1 c2]
+
+                  ["nominal" "nominal"]
+                  ;; Column with fewer categories should be on x-axis.
+                  (if (>= (get options-count c1)
+                          (get options-count c2))
+                    [c2 c1]
+                    [c1 c2]))]
+    ;; Now deal with c1-c2 whether they have been reversed or not.
+    (let [c1-type (vega-type schema c1)
+          c2-type (vega-type schema c2)]
+      {:mark {:type "rect" :tooltip true}
+       :width (case c1-type
+                "nominal" {:step 20},
+                "quantitative" 240)
+       :height (case c2-type
+                "nominal" {:step 20},
+                "quantitative" 240),
+       :encoding {:x {:bin (case c1-type
+                             "nominal" false
+                             "quantitative" {:maxbins 50
+                                             :extent (get ranges c1)})
+                      :field (name c1),
+                      :type c1-type}
+                  :y {:bin (case c2-type
+                             "nominal" false
+                             "quantitative" {:maxbins 50}),
+                                             :extent (get ranges c2)
+                      :field (name c2),
+                      :type c2-type}
+                  :color {:aggregate "count",
+                          :type "quantitative"
+                          :legend nil}}})))
 
 (defn spec
   [columns num-columns]

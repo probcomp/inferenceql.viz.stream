16687644a28a6f612bf7c20d314874b7301d8a9d
Harish Tella <htella@mit.edu>
2021-12-16 19:42:13 -0600

    remove unneeded scale sections

--- src/inferenceql/viz/stream/panels/viz/inferences.cljs
+++ src/inferenceql/viz/stream/panels/viz/inferences.cljs
@@ -70,19 +70,13 @@
                              "quantitative" {:maxbins 50
                                              :extent (get ranges c1)})
                       :field (name c1),
-                      :type c1-type
-                      :scale (case c1-type
-                               "nominal" {}
-                               "quantitative" {:domain (get ranges c1)})}
+                      :type c1-type}
                   :y {:bin (case c2-type
                              "nominal" false
                              "quantitative" {:maxbins 50
                                              :extent (get ranges c2)})
                       :field (name c2),
-                      :type c2-type
-                      :scale (case c2-type
-                               "nominal" {}
-                               "quantitative" {:domain (get ranges c2)})}
+                      :type c2-type}
                   :color {:aggregate "count",
                           :type "quantitative"
                           :legend nil}}})))
